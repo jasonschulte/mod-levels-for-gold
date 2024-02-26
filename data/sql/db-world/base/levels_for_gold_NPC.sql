727e3f3fb6d9e49b318f4efca58203164e3de00d
Jason Schulte <jasonschulte@gmail.com>
2024-02-26 17:46:44 -0600

    update subname

--- data/sql/db-world/base/levels_for_gold_NPC.sql
+++ data/sql/db-world/base/levels_for_gold_NPC.sql
@@ -3,4 +3,4 @@ SET
 @Name = "Treets";
 
 REPLACE INTO `creature_template` (`entry`, `modelid1`, `modelid2`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `scale`, `rank`, `dmgschool`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `AIName`, `MovementType`, `HoverHeight`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`) VALUES
-(@Entry, 1455, 0, @Name, 'Gold for Levels', NULL, 0, 6, 6, 0, 35, 1, 0.5, 0, 0, 2000, 0, 1, 0, 10, 134217728, 0, 0, 0, '', 0, 1, 0, 0, 1, 0, 2, 'npc_levels_for_gold');
+(@Entry, 1455, 0, @Name, 'Levels for Gold', NULL, 0, 6, 6, 0, 35, 1, 0.5, 0, 0, 2000, 0, 1, 0, 10, 134217728, 0, 0, 0, '', 0, 1, 0, 0, 1, 0, 2, 'npc_levels_for_gold');
